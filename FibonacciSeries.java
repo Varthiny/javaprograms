77a64f00bf620aa95e1dbccc15791b401b3b0f30
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-07-16 22:19:15 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,21 @@
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+			 int n1=5,n2=6,i,n3,sum=20;    
+			 System.out.print(n1+" "+n2);
+			    
+			 for(i=2;i<sum;i++){
+			 
+			  n3=n1+n2;    
+			  System.out.print(" "+n3);    
+			  
+			  n1=n2;    
+			  n2=n3;    
+			 }    
+			  
+			
+	}
+
+}
