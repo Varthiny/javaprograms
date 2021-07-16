2b6f0b49c54748e3a070f0a6ea8b89ee3e4599f0
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-07-16 15:21:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int marks = 35;
+		
+		if (marks > 50) {
+			System.out.println("pass");
+		}
+		else {
+			System.out.println("fail");
+				
+		}
+	
+	}
+}
