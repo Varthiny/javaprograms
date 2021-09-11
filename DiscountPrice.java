5bceb1b67fb611ee70e6b9788254d26fcc8045df
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-09-11 15:02:47 +0530

    Update DiscountPrice.java

--- DiscountPrice.java
+++ DiscountPrice.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
  
 public class DiscountPrice {
 
-	public static void main(String args[]) {
+public static void main(String args[]) {
 	
-	double  listedPrice,discountPercentage,percentageToRupees;
+  double  listedPrice,discountPercentage,percentageToRupees;
 	
-	Scanner sc=new Scanner(System.in);
+Scanner sc=new Scanner(System.in);
 	
-	System.out.println("Enter listedprice");	
+System.out.println("Enter listedprice");	
                
-		listedPrice=sc.nextDouble();
+    listedPrice=sc.nextDouble();
  
-    System.out.println("Enter discount percentage ");	
+System.out.println("Enter discount percentage ");	
                
-		discountPercentage=sc.nextDouble();			
+    discountPercentage=sc.nextDouble();			
 	
-    percentageToRupees= (listedPrice*discountPercentage)/100; 
+percentageToRupees= (listedPrice*discountPercentage)/100; 
  
-	System.out.print("Amount after discount=");
-	System.out.print(listedPrice-percentageToRupees);
+System.out.print("Amount after discount=");
+System.out.print(listedPrice-percentageToRupees);
 	
  
 	}
