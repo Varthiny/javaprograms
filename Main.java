b6ceb6de46ac0b5e24b2d5d39227df318cc8782b
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-10-01 19:10:20 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
-
-
-
-
+package com.bankmanagement.main;
+import com.bankmanagement.pojo.*;
 public class Main {
-	
 	public static void main(String[] args) {
+		Customer custobject = new Customer();
+		custobject.setName("VishnuVarthiny");
+		System.out.println(custobject.getName());
+		custobject.setId(114282801);
+		System.out.println(custobject.getId());
+		custobject.setPhone("916382364551");
+		System.out.println(custobject.getPhone());
+		custobject.setDob("7-aug-2001");
+		System.out.println(custobject.getDob());
+		custobject.setGender("Female");
+		System.out.println(custobject.getGender());
+		custobject.setEmail("vishnuvarthiny@gamil.com");
+		System.out.println(custobject.getEmail());
+		custobject.setAddress();
+		System.out.println(custobject.getAddress());
 		
-		int marks = 35;
 		
-		if (marks > 50) {
-			System.out.println("pass");
-		}
-		else {
-			System.out.println("fail");
-				
-		}
-	
+		
 	}
+
 }
