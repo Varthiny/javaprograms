227c0c35e0ca8c6ca9d50701163f5e069ed8b44a
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-10-01 19:14:23 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,25 @@ import com.bankmanagement.pojo.*;
 public class Main {
 	public static void main(String[] args) {
 		Customer custobject = new Customer();
+		
 		custobject.setName("VishnuVarthiny");
 		System.out.println(custobject.getName());
+		
 		custobject.setId(114282801);
 		System.out.println(custobject.getId());
+		
 		custobject.setPhone("916382364551");
 		System.out.println(custobject.getPhone());
+		
 		custobject.setDob("7-aug-2001");
 		System.out.println(custobject.getDob());
+		
 		custobject.setGender("Female");
 		System.out.println(custobject.getGender());
+		
 		custobject.setEmail("vishnuvarthiny@gamil.com");
 		System.out.println(custobject.getEmail());
+		
 		custobject.setAddress();
 		System.out.println(custobject.getAddress());
 		
