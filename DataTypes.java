ecb9a99fe965bff565fdfe7a7414017b7e1d4d6c
Varthiny <87259793+Varthiny@users.noreply.github.com>
2021-11-06 09:27:48 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,50 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// boolean
+		boolean b = false;
+		System.out.println("b has "+b);
+		
+		//byte
+		byte myByte = 10;
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		//short
+		short s = 10;
+		System.out.println(Short.MAX_VALUE);
+		
+		//char
+		char ch = 'v';
+		System.out.println(ch);
+		char myChar = 70;
+		System.out.println(myChar);
+		
+		//int
+		int i = 10;
+		int maxValue = Integer.MAX_VALUE;
+		int newMax = maxValue + 1;
+		System.out.println(newMax);
+		System.out.println(Integer.MAX_VALUE);
+		
+		//long
+		long l = 872398742943l;
+		System.out.println(l);
+		
+		//float
+		float f = 3.14f;
+		System.out.println(22/7f);
+		
+		//double
+		double d = 3.14;
+		System.out.println(22/7d);
+		
+		
+		
+		
+		
+		
+	}
+
+}
