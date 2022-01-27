9ca897411b1182591017344bcdfb1e8dedd2ba62
Varthiny <87259793+Varthiny@users.noreply.github.com>
2022-01-27 19:54:29 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the testcase:");
+		int t = sc.nextInt();
+		
+		while(--t >0){
+			System.out.println("enter the number:");
+		    int n = sc.nextInt();
+		    
+		    if(n<10){
+		    System.out.println("Thanks for helping Chef!");
+		    }
+		
+	    	else{
+		    System.out.println("-1");
+	    	}
+    	}
+    }
+}
